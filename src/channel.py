4269a6cb50d3e61b2465e2d13ac8e1d18cb2a312
User <yuri.ogorodnik@mail.ru>
2023-06-20 20:48:03 +0300

    Del comments

--- src/channel.py
+++ src/channel.py
@@ -1,13 +1,9 @@
 import json
 import os
-
-# необходимо установить через: pip install google-api-python-client
 from googleapiclient.discovery import build
 
-# YT_API_KEY скопирован из гугла и вставлен в переменные окружения
 api_key: str = os.getenv('YT_API_KEY')
 
-# создать специальный объект для работы с API
 youtube = build('youtube', 'v3', developerKey=api_key)
 
 
